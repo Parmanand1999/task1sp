6b82b46ae8fd24819706e33f21d5f9d6157e123b
parmanand <parmanandmaurya1999@.com>
2023-02-20 19:03:11 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './Componant/componant3.css'
+import './App.css';
+
+import Componant2 from './Componant/Componant2';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Componant2 />
+    </div>
+  );
+}
+
+export default App;
